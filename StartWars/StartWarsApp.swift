ff7f5771661ac229d7b456747705cb6f88ad6d1e
Maria Wilfling <m.wilfling@web.de>
2023-11-21 10:23:00 +0100

    Initial Commit

---
+++ StartWars/StartWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartWarsApp.swift
+//  StartWars
+//
+//  Created by Maria Wilfling on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct StartWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
